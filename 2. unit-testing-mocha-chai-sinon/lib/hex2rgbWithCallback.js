17d031f1a5cd60fc3aa16b8d446afa8626d6cd43
p-12s <mental.notes@yandex.ru>
2019-03-08 19:08:34 +0300

    Добавил пример тестирования модуля с колбеком

---
+++ 2. unit-testing-mocha-chai-sinon/lib/hex2rgbWithCallback.js
@@ -0,0 +1,26 @@
+module.exports = function(hex, callback) {
+
+    setTimeout(function() {
+        if (/^#/.test(hex)) {
+            hex = hex.slice(1);
+        }
+
+        if (hex.length !== 3 && hex.length !== 6) {
+            callback( new Error('Invalid hexademical string') );
+            return;
+        }
+
+        var nums = hex.split('');
+
+        if (nums.length === 3) {
+            nums = [nums[0], nums[0], nums[1], nums[1], nums[2], nums[2]];
+        }
+
+        var r = parseInt([nums[0], nums[1]].join(''), 16);
+        var g = parseInt([nums[4], nums[5]].join(''), 16);
+        var b = parseInt([nums[2], nums[3]].join(''), 16);
+
+        callback(null, [r, g, b]);
+    }, 50);
+
+};
---
+++ 2. unit-testing-mocha-chai-sinon/test/3-hex2rgbWithCallback.test.js
@@ -0,0 +1,23 @@
+var hex2rgb = require('../lib/hex2rgbWithCallback');
+var assert = require('assert');
+
+describe('hex2rgbWithCallback', function() {
+
+    it('should throw an error if the value is not a hex code', function(done) {
+
+        hex2rgb('blue', function(error, result) {
+            assert(error);
+            done();
+        });
+    });
+
+    it('should return a correctly converted rgb value', function(done) {
+        hex2rgb('#fff', function(error, result) {
+            assert.strictEqual(error, null);
+            assert.deepEqual(result, [255, 255, 255]);
+            done();
+        });
+
+
+    });
+});
