8e70d70ca1bd701a689a218600effc90fa52acf7
p-12s <mental.notes@yandex.ru>
2019-03-08 18:44:01 +0300

    Добавил входной параметр, благодаря которому можно заставить вернуть модуль невалидный ответ и проверить выбросит ли он исключение

--- 2. unit-testing-mocha-chai-sinon/lib/getPalette.js
+++ 2. unit-testing-mocha-chai-sinon/lib/getPalette.js
@@ -2,8 +2,10 @@ function getData() {
     return ['#ddd', '#ff5512', '#75d709'];
 }
 
-module.exports = function() {
-    var palette = getData();
+module.exports = function(fetch) {
+
+    fetch = fetch || getData();
+    var palette = fetch;
 
     if (!Array.isArray(palette)) {
         throw new Error('Palette is not an array');
--- 2. unit-testing-mocha-chai-sinon/test/1-getPalette.test.js
+++ 2. unit-testing-mocha-chai-sinon/test/1-getPalette.test.js
@@ -4,12 +4,17 @@ var assert = require('assert');
 describe('getPalette', function() {
 
     it('should throw an error if the result is not an array', function() {
+
+        function fetch() {
+            return 'not empty';
+        }
+
         assert.throws(function(){
-            getPalette()
+            getPalette(fetch)
         }, /Palette is not an array/);
     });
 
-    it('should return an array with 3 items', function() {
+    it('should return an array with 3 items by default', function() {
         var palette = getPalette();
 
         assert(Array.isArray(palette));
